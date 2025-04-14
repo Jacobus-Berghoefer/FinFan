15745c3d1274458a35c219ba50d24a8e11a6b0bf
Jacobus <jacobus.berghoefer@gmail.com>
2025-04-14 16:22:35 -0500

    cookie parser install

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,7 +10,7 @@ import matchupRoutes from './routes/api/matchup-routes.js';
 import betRoutes from './routes/api/bet-routes.js';
 import payoutRoutes from './routes/api/payout-routes.js';
 import sideBetRoutes from './routes/api/sidebet-routes.js';
-
+import cookieParser from 'cookie-parser';
 
 const forceDatabaseRefresh = false;
 const app = express();
@@ -26,12 +26,14 @@ const __dirname = path.dirname(__filename);
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api', routes); // ✅ Only mount API routes under /api
 app.use('/api', buyinRoutes);
 app.use('/api', matchupRoutes);
 app.use('/api', betRoutes);
 app.use('/api', payoutRoutes);
 app.use('/api', sideBetRoutes);
+
 
 // Static File Serving
 const clientBuildPath = path.join(__dirname, "../../client/dist");
